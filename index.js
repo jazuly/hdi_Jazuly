2d7d57e7684ae3cd224207c7eb7bf74b523aefd3
jazuly <ajajazuly@gmail.com>
2021-04-08 22:42:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const guestPattern = function(value) {
 	
-    if(!(/\d/g).test(value)) return console.log('Invalid Data!');
+    if(!(/^\d*$/).test(value)) return console.log('Invalid Data!');
     if(value < -5) return console.log('Data too small!');
     if(value < 1) return console.log('');
     if(value == 1) return console.log('x');
     if(value > 20) return console.log('Data too big!');
   
   
-      let hLoop = value * 2;
+    let hLoop = value * 2;
     let text = '';
     let newText = '';
     let finalData = [];
@@ -24,25 +24,62 @@ const guestPattern = function(value) {
     //Delete first string and add new string at the end string
     for(let i=0; i< hLoop; i++) {
       let last3 = text.substr(text.length - 3);
-      if(last3 == '...') {
-        text = text.substr(1)
-        text += 'x'
-      } else if(last3 == '..x') {
-        text = text.substr(1)
-        text += '.'
-      } else if(last3 == '.x.') {
-        text = text.substr(1)
-        text += 'o'
-      } else if(last3 == 'x.o') {
-        text = text.substr(1)
-        text += '.'
-      } else if (last3 == '.o.') {
-        text = text.substr(1)
-        text += 'x'
-      } else if (last3 == 'o.x') {
-        text = text.substr(1)
-        text += '.'
+      
+      switch(last3) {
+      	case '...':
+        	text = text.substr(1)
+        	text += 'x'
+          break;
+          
+         case '..x':
+         	text = text.substr(1)
+        	text += '.'
+          break;
+          
+         case '.x.':
+         	text = text.substr(1)
+        	text += 'o'
+          break;
+          
+         case 'x.o':
+         	text = text.substr(1)
+        	text += '.'
+          break;
+          
+         case '.o.':
+         	text = text.substr(1)
+        	text += 'x'
+          break;
+          
+         case 'o.x':
+         	text = text.substr(1)
+        	text += '.'
+          break;
+          
+         default:
+         	console.log('Failed Build Data!');
+          break;
       }
+      
+      /* if(last3 == '...') {
+         text = text.substr(1)
+         text += 'x'
+       } else if(last3 == '..x') {
+         text = text.substr(1)
+         text += '.'
+       } else if(last3 == '.x.') {
+         text = text.substr(1)
+         text += 'o'
+       } else if(last3 == 'x.o') {
+         text = text.substr(1)
+         text += '.'
+       } else if (last3 == '.o.') {
+         text = text.substr(1)
+         text += 'x'
+       } else if (last3 == 'o.x') {
+         text = text.substr(1)
+         text += '.'
+       } */
     }
   
   
@@ -69,12 +106,14 @@ const guestPattern = function(value) {
       console.log(fFloor.replaceAll('.', ' '))
     }
   
-  
+  	
+    //Replace all dot to space
     //Print Middle Floor
     let mFloor = text.replaceAll('.', ' ')
     console.log(mFloor)
   
-  
+  	//Convert string to array and reverse the string and convert it to string again
+    //Replace all x,o,and dot to space
     //Print Second Floor
     for (let i=0; i< finalData.length; i++) {
       let sFloor = finalData[i].split("").reverse().join("");
@@ -82,4 +121,4 @@ const guestPattern = function(value) {
     }
   }
   
-  guestPattern(1)
+  guestPattern('10')
